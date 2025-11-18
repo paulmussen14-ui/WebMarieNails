3925c96b159a5cca7518d8cbdd4fab54db82a83b
Jean Paul Moncada <paulmussen14@gmail.com>
2025-11-18 04:25:14 -0500

    Fix: make build.sh executable + update Procfile

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: gunicorn Web_MarieNails.wsgi:application
+
+
--- Web_MarieNails/settings.py
+++ Web_MarieNails/settings.py
@@ -1,6 +1,6 @@
-
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -93,6 +94,13 @@ STATICFILES_DIRS = [
     BASE_DIR / "core/static",
 ]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ BASE_DIR / 'core/static' ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 
 # Internationalization
@@ -111,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
