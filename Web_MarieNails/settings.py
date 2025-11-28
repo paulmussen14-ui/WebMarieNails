80c6435e5c8e56ca11440e8ee552223eb8bd68fd
Jean Paul Moncada <paulmussen14@gmail.com>
2025-11-28 01:50:45 -0500

    Renombrar archivos estáticos y actualizar referencias en HTML

--- Web_MarieNails/settings.py
+++ Web_MarieNails/settings.py
@@ -1,8 +1,18 @@
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
+
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dom&zh^g6)px-qj#u9*s-g-72p#hq4mm)vm=6yv8!70ha*7vf_')
@@ -13,6 +23,9 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
+
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -54,15 +67,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Web_MarieNails.wsgi.application'
 
+
+
 # Database
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
+    
 }
 
 # Password validation
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -82,7 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'es-pe'
 TIME_ZONE = 'America/Lima'
 USE_I18N = True
+
+
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
@@ -90,7 +115,11 @@ STATICFILES_DIRS = [BASE_DIR / 'core/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 # Default primary key field type
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
